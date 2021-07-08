366200d2996692ed43106df99d59ff70778385ed
Deryk DeGuzman <deryk.deguzman@gmail.com>
2021-07-08 11:28:17 -0700

    remove extra /search from start of search path

--- www/api_setup.py
+++ www/api_setup.py
@@ -1,4 +1,4 @@
 from www.features.player_search.resources import PlayerSearch
 from www.global_dependencies import global_dependencies
 
-global_dependencies.api.add_resource(PlayerSearch, '/search/player/<search_string>')
+global_dependencies.api.add_resource(PlayerSearch, '/player/<search_string>')
