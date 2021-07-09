20fdea58646e3c4cc8f13a532ecff5aafb12183a
Deryk DeGuzman <deryk.deguzman@gmail.com>
2021-07-08 17:15:29 -0700

    return player results with active teams only

--- www/features/player_search/serializers.py
+++ www/features/player_search/serializers.py
@@ -5,4 +5,4 @@ def compile_search_results_from_player_data(player_data=[]):
     'team_code': p.team_code,
     'key': p.key,
     'img_url': p.img_url,
-  } for p in player_data]
+  } for p in player_data if p.team_code]
